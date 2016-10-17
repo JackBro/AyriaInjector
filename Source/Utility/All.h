b2e12aded531a0f4383065572d3e56100b9b6ec8
Convery <convery10122@gmail.com>
2016-10-17 14:44:11 +0200

    Initial  commit.

---
+++ Source/Utility/All.h
@@ -0,0 +1,14 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: GPL 2.0
+    Started: 2016-10-6
+    Notes:
+        Includes all utility classes.
+*/
+
+#pragma once
+
+#include "Crypto/FNV1.h"
+#include "Files/Filesystem.h"
+#include "Strings/Debugstring.h"
+#include "Strings/Variadicstring.h"
